7f5cfb14c8b300fd3f2d4b6efbb058bd6b136102
PraneethReddy2701 <praneeth.kotholla@gmail.com>
2024-08-10 15:33:36 +0530

    Update file_explorer.h

--- include/file_explorer.h
+++ include/file_explorer.h
@@ -7,7 +7,7 @@
 class FileExplorer {
 public:
     explicit FileExplorer(Logger& logger);
-    
+
     void listFiles(const std::string& directory);
     bool copyFile(const std::string& src, const std::string& dest);
     bool moveFile(const std::string& src, const std::string& dest);
@@ -15,9 +15,12 @@ public:
     bool createFile(const std::string& path);
     bool searchFile(const std::string& filename);
     bool setPermissions(const std::string& path, int permissions);
+    void changeDirectory(const std::string& directory);
+    void goUpOneDirectory();
 
 private:
     Logger& logger_;
+    std::string currentDirectory_;
 };
 
 #endif // FILEEXPLORER_H
